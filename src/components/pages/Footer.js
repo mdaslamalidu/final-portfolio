375a1af821fb8f51ba420bf85b358a5b853fea48
mdaslamalidu <thehometutor2022@gmail.com>
2023-01-14 21:52:15 +0600

    update footer

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -3,7 +3,7 @@ import SocialIcons from "../../hooks/SocialIcons";
 const Footer = () => {
   return (
     <footer className="h-64 bg-[#0A1929] pt-10 mt-5">
-      <div className="w-10/12 mx-auto">
+      <div className="w-5/6 mx-auto">
         <SocialIcons />
         <div className="md:flex justify-center md:justify-between text-center ">
           <p className="font-playfair font-semibold text-2xl text-yellow">
